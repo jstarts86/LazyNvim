0d91127dac8dcda6e6b05a5e433903f0c1a3140d
John Song <jstarts86@gmail.com>
2023-12-25 16:07:50 -0600

    Initializing LazyVim config

---
+++ lua/config/options.lua
@@ -0,0 +1,11 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.guicursor = ""
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.g.autoformat = false
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = { transparent = true, styles = { sidebars = "transparent", floats = "transparent" } },
+  },
+}
