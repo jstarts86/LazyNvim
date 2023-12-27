a4e4aedf03b2abcf65f02f6ce50ef308279e886d
jstarts86 <jstarts86@gmail.com>
2023-12-27 17:43:56 -0600

    solarized osaka keymaps

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,11 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    opts = { transparent = true, styles = { sidebars = "transparent", floats = "transparent" } },
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+      styles = { sidebars = "transparent", floats = "transparent" },
+    },
   },
 }
