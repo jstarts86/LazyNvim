0da776549470ca9ad6f8d1b349f1bc06c597a989
John Song <jstarts86@gmail.com>
2023-12-29 23:27:37 -0600

    Added some Vim options and undo file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,3 +9,11 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.g.autoformat = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+vim.opt.scrolloff = 9
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
