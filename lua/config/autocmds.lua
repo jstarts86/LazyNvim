6ee400f77779385fd246963d21d9722e7b3357f7
John Song <jstarts86@gmail.com>
2024-01-08 23:32:51 -0600

    Changed the tab spacing for html css js and ts

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,10 +2,18 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "json", "jsonc" },
   callback = function()
     vim.wo.conceallevel = 0
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "html", "css", "js", "ts" },
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
   end,
 })
