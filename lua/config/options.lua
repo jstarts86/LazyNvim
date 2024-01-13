06b785bc90da8db3f49225598a20ed1c8e18a4ac
John Song <jstarts86@gmail.com>
2024-01-13 12:42:18 -0600

    vim.opt.undofile

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -13,6 +13,7 @@ vim.g.autoformat = false
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.scrolloff = 9
 vim.opt.updatetime = 50
