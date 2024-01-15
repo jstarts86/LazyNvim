eb1d964e14a99d62782868a2397f1de1a8151a0d
John Song <jstarts86@gmail.com>
2024-01-15 00:47:42 -0600

    Deleted telescope undo and added undo-tree

---
+++ lua/plugins/undo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "mbbill/undotree",
+    keys = {
+      { -- lazy style key map
+        "<leader>o",
+        "<cmd>UndotreeToggle<CR>",
+        desc = "undo history",
+      },
+    },
+  },
+}
