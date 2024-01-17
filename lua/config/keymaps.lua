ae9d3f1f17242d579f9427cbb86c2ee87ea313b9
John Song <jstarts86@gmail.com>
2024-01-16 22:09:58 -0600

    auto complete done. Refactor trial but failed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,17 @@
 --
 
 local keymap = vim.keymap
-local opts = { noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
+keymap.set("x", "<leader>re", ":Refactor extract ")
+keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+
+keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+
+keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+
+keymap.set( "n", "<leader>rI", ":Refactor inline_func")
+
+keymap.set("n", "<leader>rb", ":Refactor extract_block")
+keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
 
--- lua/plugins/refactor.lua
+++ lua/plugins/refactor.lua
@@ -6,52 +6,7 @@ return {
       {
         "<leader>r",
         desc = "refactor",
-      },
-      {
-        "x",
-        "<leader>re",
-        "<cmd>Refactor extract<CR>",
-        desc = "visual extract",
-      },
-      {
-        "x",
-        "<leader>rf",
-        "<cmd>Refactor extract_to_file<CR>",
-        desc = "Visual extract to file",
-      },
-      {
-        "x",
-        "<leader>r",
-        "<cmd>Refactor extract_var<CR>",
-        desc = "visual extract var",
-      },
-      {
-        "x",
-        "<leader>ri",
-        "<cmd>Refactor inline_var<CR>",
-        desc = "refactor inline var",
-      },
-      {
-        "x",
-        "<leader>ri",
-        "<cmd>Refactor inline_var<CR>",
-        desc = "refactor inline var",
-      },
-      {
-        "<leader>ri",
-        "<cmd>Refactor inline_func<CR>",
-        desc = "refactor inline func",
-      },
-      {
-        "<leader>rb",
-        "<cmd>Refactor extract block",
-        desc = "extract_block",
-      },
-      {
-        "<leader>rbf",
-        "<cmd>Refactor extract_block_to_file",
-        desc = "extract block to file",
-      },
+      }
     },
     wants = { "telescope.nvim" },
     dependencies = {
