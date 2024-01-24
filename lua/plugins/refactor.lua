14fb26d1c9307300e439fb6c3a7b8779b55c0272
jstarts86 <jstarts86@gmail.com>
2024-01-23 20:34:46 -0500

    disabled refactor

--- lua/plugins/refactor.lua
+++ lua/plugins/refactor.lua
@@ -1,20 +1,20 @@
-return {
-  {
-    "ThePrimeagen/refactoring.nvim",
-    module = { "refactoring", "telescope" },
-    keys = {
-      {
-        "<leader>r",
-        desc = "refactor",
-      }
-    },
-    wants = { "telescope.nvim" },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    config = function()
-      require("config.refactoring").setup()
-    end,
-  },
-}
+-- return {
+--   {
+--     "ThePrimeagen/refactoring.nvim",
+--     module = { "refactoring", "telescope" },
+--     keys = {
+--       {
+--         "<leader>r",
+--         desc = "refactor",
+--       }
+--     },
+--     wants = { "telescope.nvim" },
+--     dependencies = {
+--       "nvim-lua/plenary.nvim",
+--       "nvim-treesitter/nvim-treesitter",
+--     },
+--     config = function()
+--       require("config.refactoring").setup()
+--     end,
+--   },
+-- }
