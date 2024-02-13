f589d939a81de54a93e9cb75e356ed5394334f3d
John Song <jstarts86@gmail.com>
2024-02-12 23:45:14 -0600

    rose pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,21 @@
 return {
-{
-    "craftzdog/solarized-osaka.nvim",
-    opts = {
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent"
-            },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      vim.cmd("colorscheme rose-pine")
+      require("rose-pine").setup({
+        disable_backround = true,
+        styles = {
+          transparency = true,
         },
-    },
+      })
+    end,
+  },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "solarized-osaka",
+            colorscheme = "rose-pine",
         },
-    },
-
+    }
 }
---
+++ lua/plugins/solarized-osaka.lua
@@ -0,0 +1,18 @@
+return {
+-- {
+--     "craftzdog/solarized-osaka.nvim",
+--     opts = {
+--             transparent = true,
+--             styles = {
+--                 sidebars = "transparent",
+--                 floats = "transparent"
+--             },
+--         },
+--     },
+--     {
+--         "LazyVim/LazyVim",
+--         opts = {
+--             colorscheme = "solarized-osaka",
+--         },
+--     },
+}
