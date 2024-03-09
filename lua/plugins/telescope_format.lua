dafdeea101f73f9fc38ba324b8ea8853b32790bf
John Song <jstarts86@gmail.com>
2024-03-09 21:46:07 +0900

    added anaconda telescope

--- lua/plugins/telescope_format.lua
+++ lua/plugins/telescope_format.lua
@@ -1,9 +1,29 @@
-return {
 
-  {
+return {
+ {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "IllustratedMan-code/telescope-conda.nvim",
+    },
+    config = function()
+      require('telescope').setup {
+        extensions = {
+          conda = {
+            anaconda_path = "/opt/anaconda3"
+          }
+        }
+      }
+    end,
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files (cwd)" },
+      {
+        "<leader>fa",
+        desc = "choose the anaconda environment",
+        function()
+          require("telescope").extensions.conda.conda({})
+        end,
+      },
     },
-  },
+ },
 }
