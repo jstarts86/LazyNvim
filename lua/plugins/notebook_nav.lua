189165f5e209fde30919e5eb1335d21299a4adb4
John Song <jstarts86@gmail.com>
2024-03-25 22:51:52 +0900

    added image support and changed to kitty

--- lua/plugins/notebook_nav.lua
+++ lua/plugins/notebook_nav.lua
@@ -19,9 +19,9 @@ return {
     },
     dependencies = {
       "echasnovski/mini.comment",
-      "hkupty/iron.nvim", -- repl provider
+      -- "hkupty/iron.nvim", -- repl provider
       -- "akinsho/toggleterm.nvim", -- alternative repl provider
-      -- "benlubas/molten-nvim", -- alternative repl provider
+      "benlubas/molten-nvim", -- alternative repl provider
       "anuvyklack/hydra.nvim",
     },
     event = "VeryLazy",
