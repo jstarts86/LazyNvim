abc43e4c67c5d2a7c86c6bb5388b2d126b663cf1
John Song <jstarts86@gmail.com>
2025-09-20 15:02:24 +0900

    trying to do proper snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,63 +1,67 @@
 return {
   -- lazy.nvim
-  -- {
-  --   "folke/snacks.nvim",
-  --   -- lazy = false,
-  --   opts = {
-  --     picker = {
-  --       sources = {
-  --         explorer = {
-  --           enabled = true,
-  --           layout = {
-  --             preset = "sidebar",
-  --             preview = false,
-  --             layout = {
-  --               position = "right",
-  --             },
-  --           },
-  --           auto_close = true,
-  --         },
-  --       },
-  --       win = {
-  --         input = {
-  --           keys = {
-  --             ["<A-h>"] = { "toggle_hidden", mode = { "i", "n" } },
-  --             ["<A-i>"] = { "toggle_ignored", mode = { "i", "n" } },
-  --           },
-  --         },
-  --         list = {
-  --           keys = {
-  --             ["<A-h>"] = "toggle_hidden",
-  --             ["<A-i>"] = "toggle_ignored",
-  --             ["<C-l>"] = "focus_preview",
-  --           },
-  --         },
-  --         preview = {
-  --           keys = {
-  --             ["<C-l>"] = "focus_preview",
-  --           },
-  --         },
-  --       },
-  --     },
-  --
-  --     indent = {
-  --       enabled = false,
-  --     },
-  --     buffers = {
-  --       enabled = true,
-  --     },
-  --     words = {
-  --       enabled = true,
-  --     },
-  --     keys = {
-  --       {
-  --         "<leader>uC",
-  --         function()
-  --           Snacks.picker("colorschemes") -- ✅ call picker by name
-  --         end,
-  --         desc = "Colorschemes",
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "folke/snacks.nvim",
+    -- lazy = false,
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            enabled = true,
+            layout = {
+              preset = "sidebar",
+              preview = false,
+              layout = {
+                position = "right",
+              },
+            },
+            auto_close = true,
+          },
+        },
+        win = {
+          input = {
+            keys = {
+              ["<C-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+              ["<C-e>"] = { "toggle_ignored", mode = { "i", "n" } },
+              ["<C-l>"] = { "focus_preview", mode = { "i", "n" } },
+              ["<C-w>"] = { "cycle_win", mode = { "i", "n" } },
+            },
+          },
+          list = {
+            keys = {
+              ["<C-h>"] = "toggle_hidden",
+              ["<C-e>"] = "toggle_ignored",
+              ["<C-l>"] = "focus_preview",
+              ["<C-w>"] = "cycle_win",
+            },
+          },
+          preview = {
+            keys = {
+              ["<C-l>"] = "focus_preview",
+              ["<C-w>"] = "cycle_win",
+            },
+          },
+        },
+      },
+
+      indent = {
+        enabled = false,
+      },
+      buffers = {
+        enabled = true,
+      },
+      words = {
+        enabled = true,
+      },
+      keys = {
+        {
+          "<leader>uC",
+          function()
+            Snacks.picker("colorschemes") -- ✅ call picker by name
+          end,
+          desc = "Colorschemes",
+        },
+      },
+    },
+  },
 }
