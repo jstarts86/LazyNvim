46072717fa8ac416db5ddd5131aed9e572458f8c
John Song <jstarts86@gmail.com>
2026-07-06 12:37:42 -0500

    harpoon updated to ctrl + number

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -30,8 +30,17 @@ return {
 
     for i = 1, 9 do
       table.insert(keys, {
-        "<leader>" .. i,
+        "<C-" .. i .. ">",
         function()
+          if vim.bo.filetype == "neo-tree" then
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              local buf = vim.api.nvim_win_get_buf(win)
+              if vim.bo[buf].filetype ~= "neo-tree" then
+                vim.api.nvim_set_current_win(win)
+                break
+              end
+            end
+          end
           require("harpoon"):list():select(i)
         end,
         desc = "Harpoon to File " .. i,
